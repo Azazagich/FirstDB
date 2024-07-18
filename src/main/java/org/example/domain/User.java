3a4ef8bd86999a9a18e878c73911d494be0239ef
Azazagich <qwertyu2289@ukr.net>
2024-07-18 15:47:33 +0300

    add: add classes domain

---
+++ src/main/java/org/example/domain/User.java
@@ -0,0 +1,101 @@
+package org.example.domain;
+
+import java.util.Objects;
+
+/**
+ * The User class represents a user in the system.
+ * A user has a unique ID, a first name, an email, a password, and an associated role.
+ */
+public class User {
+
+    private int id;
+    private String firstName;
+    private String email;
+    private String password;
+    private Role role;
+
+    public User(){ }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User id(int id){
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public User firstName(String firstName){
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public User email(String email){
+        this.email = email;
+        return this;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public User password(String password){
+        this.password = password;
+        return this;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(firstName, user.firstName) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, email, password);
+    }
+}
